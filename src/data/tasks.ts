91bf0f5df27ffc3c04977d74f58becc09d2c3034
Arthur Rabelo <arthurrabelopro@gmail.com>
2024-01-07 17:01:54 -0300

    (setup) setup initial environment for developing the task manager app

---
+++ src/data/tasks.ts
@@ -0,0 +1,39 @@
+const tasks = [
+  {
+    name: 'Website design',
+    description: 'Define the style guide, branding and create the webdesign on Figma.',
+    completed: true
+  },
+  {
+    name: 'Website development',
+    description: 'Develop the portfolio website using Vue JS.',
+    completed: false
+  },
+  {
+    name: 'Hosting and infrastructure',
+    description: 'Define hosting, domain and infrastructure for the portfolio website.',
+    completed: false
+  },
+  {
+    name: 'Composition API',
+    description: 'Learn how to use the composition API and how it compares to the options API.',
+    completed: true
+  },
+  {
+    name: 'Pinia',
+    description: 'Learn how to setup a store using Pinia.',
+    completed: true
+  },
+  {
+    name: 'Groceries',
+    description: 'Buy rice, apples and potatos.',
+    completed: false
+  },
+  {
+    name: 'Bank account',
+    description: 'Open a bank account for my freelance business.',
+    completed: false
+  }
+]
+
+export { tasks }
